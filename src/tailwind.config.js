548b74d444b55f309c8473187cf3da305935b619
James <james.auble@gmail.com>
2023-02-15 21:34:22 -0800

    removed css vars dependent tailwind config. replaced some in file styles with tailwind. added some tailwind utilities to solve complex selector

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,10 +8,7 @@ const path = require('path')
 let plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: [
-    path.resolve(__dirname, '**/*.js'),
-    path.resolve(__dirname, '../shopify/**/*.liquid')
-  ],
+  content: [path.resolve(__dirname, '**/*.js'), path.resolve(__dirname, '../shopify/**/*.liquid')],
   theme: {
     container: {
       center: true,
@@ -44,65 +41,18 @@ module.exports = {
         'open-sans': ['"Open Sans"', 'sans-serif'],
       },
       colors: {
-        primary: {
-          50: 'var(--color-primary-50)',
-          100: 'var(--color-primary-100)',
-          200: 'var(--color-primary-200)',
-          300: 'var(--color-primary-300)',
-          400: 'var(--color-primary-400)',
-          500: 'var(--color-primary-500)',
-          600: 'var(--color-primary-600)',
-          700: 'var(--color-primary-700)',
-          800: 'var(--color-primary-800)',
-          900: 'var(--color-primary-900)',
-        },
-        secondary: {
-          50: 'var(--color-secondary-50)',
-          100: 'var(--color-secondary-100)',
-          200: 'var(--color-secondary-200)',
-          300: 'var(--color-secondary-300)',
-          400: 'var(--color-secondary-400)',
-          500: 'var(--color-secondary-500)',
-          600: 'var(--color-secondary-600)',
-          700: 'var(--color-secondary-700)',
-          800: 'var(--color-secondary-800)',
-          900: 'var(--color-secondary-900)',
-        },
-        accent: {
-          50: 'var(--color-accent-50)',
-          100: 'var(--color-accent-100)',
-          200: 'var(--color-accent-200)',
-          300: 'var(--color-accent-300)',
-          400: 'var(--color-accent-400)',
-          500: 'var(--color-accent-500)',
-          600: 'var(--color-accent-600)',
-          700: 'var(--color-accent-700)',
-          800: 'var(--color-accent-800)',
-          900: 'var(--color-accent-900)',
-        },
-        neutral: {
-          50: 'var(--color-neutral-50)',
-          100: 'var(--color-neutral-100)',
-          200: 'var(--color-neutral-200)',
-          300: 'var(--color-neutral-300)',
-          400: 'var(--color-neutral-400)',
-          500: 'var(--color-neutral-500)',
-          600: 'var(--color-neutral-600)',
-          700: 'var(--color-neutral-700)',
-          800: 'var(--color-neutral-800)',
-          900: 'var(--color-neutral-900)',
-        },
-        base: {
-          50: 'var(--color-base-50)',
-          100: 'var(--color-base-100)',
-          200: 'var(--color-base-200)',
-          300: 'var(--color-base-300)',
-          400: 'var(--color-base-400)',
-          500: 'var(--color-base-500)',
-          600: 'var(--color-base-600)',
-          700: 'var(--color-base-700)',
-          800: 'var(--color-base-800)',
-          900: 'var(--color-base-900)',
+        'cloud-burst': {
+          DEFAULT: '#252A59',
+          50: '#7179C4',
+          100: '#636CBE',
+          200: '#4953AF',
+          300: '#3D4593',
+          400: '#313876',
+          500: '#252A59',
+          600: '#151731',
+          700: '#04050A',
+          800: '#000000',
+          900: '#000000',
         },
       },
     },
@@ -112,5 +62,5 @@ module.exports = {
     plugin(function ({ addVariant }) {
       addVariant('scrolled', '.scrolled &'), addVariant('mobile-menu-open', '.mobile-menu-open &')
     }),
-  ]
+  ],
 }
