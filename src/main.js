56daa3667bc21a1557b54e4a8b874da6006c6a8b
James <james.auble@gmail.com>
2023-02-16 18:56:13 -0800

    part 2

--- src/main.js
+++ src/main.js
@@ -56,8 +56,4 @@ for (const path in alpineMagic) {
 
 Alpine.start()
 
-<<<<<<< HEAD
-console.log('a second logging part 2')
-=======
 console.log('gonna merge using rebase part 1')
->>>>>>> e9d0506 (part 1)
