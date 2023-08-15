0fa0a01d873dc7e2f9d96492e8008336b24f47d1
James <james.auble@gmail.com>
2023-08-15 22:37:44 +0900

    refactored x-scrolled

--- src/alpine/plugins/scrolled.js
+++ src/alpine/plugins/scrolled.js
@@ -1,7 +1,3 @@
-/**
- * Registers an Alpine directive that adds a class to an element when it is scrolled.
- * @param {Object} Alpine - The Alpine instance.
- */
 export default function (Alpine) {
   Alpine.directive('scrolled', (
     el,
@@ -9,79 +5,36 @@ export default function (Alpine) {
     { Alpine, effect, cleanup, evaluate, evaluateLater }
   ) => {
 
-    /**
-     * The default configuration for the directive.
-     * @type {Object}
-     * @property {string} className - The class name to add to the element when it is scrolled.
-     */
     const defaultConfig = {
       className: 'scrolled'
     }
 
-    /**
-     * Gets the configuration for the directive.
-     * @type {Function}
-     * @returns {Object} - The configuration object.
-     */
-    const getConfig = expression ? evaluateLater(expression) : () => defaultConfig
+    const getConfig = expression ? evaluateLater(expression) : (cb) => cb(defaultConfig)
+    const eventTarget = modifiers.includes('self') ? el : window
 
-    /**
-     * The event target to listen for scroll events on.
-     * @type {Object}
-     */
-    let eventTarget = window
-
-    if (modifiers.includes('self')) {
-      eventTarget = el
-    }
-
-    /**
-     * Gets the current scroll position of the window.
-     * @type {Function}
-     * @returns {number} - The scroll position.
-     */
-    const getWindowScrollY = () => {
-      return window.scrollY !== undefined
-        ? window.scrollY
-        : (document.documentElement || document.body.parentNode || document.body).scrollTop
-    }
-
-    /**
-     * The scroll event handler.
-     */
     const onScrollHandler = () => {
-      computed.scrollY = modifiers.includes('self') ? el.scrollTop : getWindowScrollY()
+      computed.scrollY = eventTarget.scrollY ?? eventTarget.scrollTop
     }
 
-    /**
-     * The reactive object that tracks the scroll position.
-     * @type {Object}
-     * @property {number} scrollY - The current scroll position.
-     */
     const computed = Alpine.reactive(
       {
-        scrollY: modifiers.includes('self') ? el.scrollTop : getWindowScrollY()
+        scrollY: eventTarget.scrollY ?? eventTarget.scrollTop
       }
     )
 
-    eventTarget.addEventListener('scroll', onScrollHandler)
+    eventTarget.addEventListener('scroll', onScrollHandler, { passive: true })
 
     cleanup(() => {
       eventTarget.removeEventListener('scroll', onScrollHandler)
     })
 
     effect(() => {
-      let config = defaultConfig
+      const action = computed.scrollY > 0 ? 'add' : 'remove'
 
       getConfig((newConfig) => {
-        config = { ...defaultConfig, ...newConfig }
+        const config = { ...defaultConfig, ...newConfig }
+        el.classList[action](config.className)
       })
-
-      if (computed.scrollY > 0) {
-        el.classList.add(config.className)
-      } else {
-        el.classList.remove(config.className)
-      }
     })
   })
 }
