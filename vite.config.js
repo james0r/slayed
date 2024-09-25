fcc9bc8d95f32052e43bd9dc3873118a19690e82
James <james.auble@gmail.com>
2024-09-25 08:43:05 -0700

    removed comments from vite config

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,11 @@ import fs from 'fs';
 import path from 'path';
 import chokidar from 'chokidar';
 
-// Utility function to copy a file
 function copyFile(src, dest) {
   fs.mkdirSync(path.dirname(dest), { recursive: true });
   fs.copyFileSync(src, dest);
 }
 
-// Utility function to remove a file
 function removeFile(dest) {
   if (fs.existsSync(dest)) {
     fs.unlinkSync(dest);
@@ -27,14 +25,12 @@ function copyPublicToAssetsPlugin() {
     name: 'vite-plugin-copy-public',
     apply: 'serve', // Only apply this during development
     configResolved(resolvedConfig) {
-      // Save the resolved Vite config
       config = resolvedConfig;
     },
     buildStart() {
       const publicDir = path.resolve(config.root, 'public');
       const assetsDir = path.resolve(config.root, 'assets');
 
-      // Watch public directory for changes
       const watcher = chokidar.watch(publicDir, { ignoreInitial: true });
 
       watcher.on('add', (filePath) => {
